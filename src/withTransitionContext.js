3e6d3dd8495ec6f1b977fa6850d15fddc8d45d6c
Andy Edwards <jedwards@fastmail.com>
2018-01-25 19:07:48 -0600

    fix(withTransitionContext): fix flow errors

--- src/withTransitionContext.js
+++ src/withTransitionContext.js
@@ -1,11 +1,12 @@
 // @flow
 
 import * as React from 'react'
+import typeof DefaultViewSlider from 'react-view-slider'
 import ViewSlider from 'react-view-slider/lib/withTransitionContext'
 import {createDrilldown} from './index'
 
 // ugly workaround for limitations of how Flow handles defaultProps
-const ViewSliderWrapper = (props: React.ElementConfig<typeof ViewSlider>) => <ViewSlider {...props} />
+const ViewSliderWrapper = (props: React.ElementConfig<DefaultViewSlider>) => <ViewSlider {...props} />
 
 export default createDrilldown({
   ViewSlider: ViewSliderWrapper,
